f9161a399134dc09de0c5071b69f64b25546fcd6
CB Sommers <cb@compute.co.il>
2026-03-18 09:28:19 +0200

    Fixed menu issues.

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6d565866'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.8odjhgcue8k"
+    "revision": "0.gknrgt9meq4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
